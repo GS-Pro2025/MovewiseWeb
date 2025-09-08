532a255bef289e99701ed19fc4e910fdaddb9e46
GS-Pro2025 <Manager-Support@gspromastermoving.com>
2025-09-08 13:14:37 -0500

    update sitemap

--- src/LandingPage/homelp.tsx
+++ src/LandingPage/homelp.tsx
@@ -41,13 +41,13 @@ const MovingWiseLanding = () => {
 
               {/* Main Heading */}
               <div className="space-y-4">
-                <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-black leading-tight drop-shadow-lg">
+                <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-white leading-tight drop-shadow-lg">
                    Smart logistic{' '}
                   <span>management</span> for your{' '}
                   <span>moving company</span>
                 </h2>
                 
-                <p className="text-lg sm:text-xl text-black max-w-md mx-auto md:mx-0 drop-shadow">
+                <p className="text-lg sm:text-xl text-white max-w-md mx-auto md:mx-0 drop-shadow">
                 With MovingWise, organize your trucks, personnel, and transfers in one place. In real time, without confusion.
                 </p>
               </div>
